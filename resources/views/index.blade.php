3d41b8e26e74cec6230bcfd0a34fdb18f11a8b4c
ejcromwell_github <elliot@eggbeater.ca>
2018-09-10 19:50:49 +0100

    Added basic bootstrap and basic styling to tables

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,51 +1,76 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Upshoot Motorbikes</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    </head>
+    <title>Upshoot Motorbikes</title>
+
     <body>
         <header>
-            Upshoot Motorbikes
+
+            <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+              <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Upshoot Motorbikes</a>
+            </nav>
+
         </header>
-        <section>
-           <h2>Count</h2>
-           <table>
-                <tr>
-                     <th>Colour</th>
-                     <th>Total</th>
-                </tr>
-                @foreach ($colour_count as $key => $value)
-                    <tr>
-                        <td>{{ $key }}</td>
-                        <td><?php echo count($value) ?></td>
-                    </tr>
-                @endforeach
-            </table>
-        </section>
+        <main class="container pt-5">
+            <section class="colour-count pt-3">
+                <h2>Motorbike Colour Count</h2>
+                <table class="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                             <th>Colour</th>
+                             <th>Total</th>
+                        </tr>
 
-        <section>
-           <h2>Owners</h2>
-           <table>
-                <tr>
-                   <th>Owner</th>
-                   <th>Brand</th>
-                   <th>Colour</th>
-                   <th>Model Year</th>
-                </tr>
+                    </thead>
+                    <tbody>
 
-                @foreach ($motorbikes as $motorbike)
-                    <tr>
-                        <td><?= ($motorbike->name) ? $motorbike->name : 'NULL'; ?></td>
-                        <td>{{ $motorbike->brand }}</td>
-                        <td>{{ $motorbike->colour }}</td>
-                        <td>{{ $motorbike->year }}</td>
-                    </tr>
-                @endforeach
-           </table>
-        </section>
+                    @foreach ($colour_count as $key => $value)
+
+                        <tr>
+                            <td>{{ $key }}</td>
+                            <td><?php echo count($value) ?></td>
+                        </tr>
+
+                    @endforeach
+
+                    </tbody>
+                </table>
+            </section>
+
+            <section class="motorbike-owners pt-3">
+                <h2>Motorbikes And Their Owners</h2>
+                <table class="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>Owner</th>
+                            <th>Brand</th>
+                            <th>Colour</th>
+                            <th>Model Year</th>
+                        </tr>
+
+                    </thead>
+                    <tbody>
+
+                    @foreach ($motorbikes as $motorbike)
+                        <tr>
+                            <td><?= ($motorbike->name) ? $motorbike->name : 'NULL'; ?></td>
+                            <td>{{ $motorbike->brand }}</td>
+                            <td>{{ $motorbike->colour }}</td>
+                            <td>{{ $motorbike->year }}</td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+            </section>
+        </main>
     </body>
 </html>
+
+
